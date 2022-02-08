8c804cee6fcf19ea46bdf2561220fbc16ffe6a3d
darinasimeonova <darrinasimeonova84@gmail.com>
2022-02-08 13:01:43 +0200

    Refactoring

--- Arrays/Obrabotka_masiv/Program.cs
+++ Arrays/Obrabotka_masiv/Program.cs
@@ -13,27 +13,13 @@ namespace Obrabotka_masiv
             {
                 string[] cmd = Console.ReadLine().Split().ToArray();
                 string command = cmd[0];
-                //if (command == "Reverse")
-                //{
-                //    input = input.Reverse().ToArray();
-                //}
-                //else if (command == "Distinct")
-                //{
-                //    input = input.Distinct().ToArray();
-                //}
-                //else if (command == "Replace")
-                //{
-                //    int index = int.Parse(cmd[1]);
-                //    string strForReplace = cmd[2];
-                //    input[index] = strForReplace;
-                //}
+                
                 switch (command)
                 {
                     case "Reverse": input = input.Reverse().ToArray(); break;
                     case "Distinct": input = input.Distinct().ToArray(); break;
                     case "Replace": input[int.Parse(cmd[1])] = cmd[2];break;
-                        //int index = int.Parse(cmd[1]);
-                        //string strForReplace = cmd[2];
+                       
                 }
             }
             Console.WriteLine(string.Join(", ", input));
